415602e1008fcbf2e81f73a10073fab0dec4c2bc
sdssix <dkdavekim@gmail.com>
2017-02-08 15:34:29 +0000

    Checkpoint 35 Validation

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+   let(:name) { RandomData.random_sentences }
+   let(:description) { RandomData.random_paragraph }
+   let(:title) { RandomData.random_sentences }
+   let(:body) { RandomData.random_paragraph }
+ # #3
+   let(:topic) { Topic.create!(name: name, description: description) }
+ # #4
+   let(:post) { topic.posts.create!(title: title, body: body) }
+ 
+   it { is_expected.to belong_to(:topic) }
+
+
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+ 
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
+   describe "attributes" do
+     it "has a title and body attribute" do
+       expect(post).to have_attributes(title: title, body: body)
+     end
+   end
+    
+end
+
