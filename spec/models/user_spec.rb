4d59e0da66012cf6e94fe6ef3c9305c4fe3c6a19
sdssix <dkdavekim@gmail.com>
2017-02-21 20:45:59 +0000

    Assignmet 36 Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,9 +21,15 @@ RSpec.describe User, type: :model do
          it "should have name and email attributes" do
            expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
          end
+         
+         it "should format the user's name" do
+            user.name = "bloc user"
+            user.save
+            expect(user.name).to eq "Bloc User"
+        end
        end
        
-       describe "invalid user" do
+    describe "invalid user" do
      let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
      let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
  
@@ -36,4 +42,5 @@ RSpec.describe User, type: :model do
      end
  
    end
+   
 end
