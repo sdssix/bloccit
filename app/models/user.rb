7c4503d56579872ba5dc52df4b1dc90f04740dd1
sdssix <dkdavekim@gmail.com>
2017-02-23 21:25:22 +0000

    Checkpoint 40 Authorization done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_many :posts
 
     before_save { self.email = email.downcase if email.present? }
+    before_save { self.role ||= :member }
 
  # #3
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -16,4 +17,7 @@ class User < ActiveRecord::Base
 
  # #6
     has_secure_password
+    
+    enum role: [:member, :admin]
+
 end
