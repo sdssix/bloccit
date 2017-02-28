245f37a5e831f4d51aee3c8221478ad365c47fe9
sdssix <dkdavekim@gmail.com>
2017-02-28 20:57:36 +0000

    Checkpoint 48 api done

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
     before_save { self.email = email.downcase if email.present? }
     before_save { self.role ||= :member }
 
+   before_create :generate_auth_token
+
+
  # #3
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
  # #4
@@ -33,6 +36,13 @@ class User < ActiveRecord::Base
      gravatar_id = Digest::MD5::hexdigest(self.email).downcase
      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
    end
-   
+
+
+   def generate_auth_token
+     loop do
+       self.auth_token = SecureRandom.base64(64)
+       break unless User.find_by(auth_token: auth_token)
+     end
+   end   
    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,15 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
+
+ 
+ # #19
+   namespace :api do
+     namespace :v1 do
+       resources :users, only: [:index, :show]
+       resources :topics, only: [:index, :show]
+       
+     end
+   end
   
 end
